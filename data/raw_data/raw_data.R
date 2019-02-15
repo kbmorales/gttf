7feb26f7ef03bf291946f91e556c00dcd7939d86
t-bell <tiana.e.bell@problemforward.com>
2019-02-15 16:16:44 +0000

    oauth token problems

--- data/raw_data/raw_data.R
+++ data/raw_data/raw_data.R
@@ -1,1 +1,13 @@
-raw data
+library(googlesheets)
+library(dplyr)
+
+token <- gs_auth("googlesheets_token.rds")
+gd_token()
+saveRDS(token, file = "googlesheets_token.rds")
+
+
+
+gs1 <- gs_url("https://docs.google.com/spreadsheets/d/1w-MVRhfk4obUF0C_YNXVUZ0UuqEz2Ov12ygmFaP58LE/edit#gid=0",
+              visibility = "private")
+ gs_read("https://docs.google.com/spreadsheets/d/1w-MVRhfk4obUF0C_YNXVUZ0UuqEz2Ov12ygmFaP58LE/edit#gid=0", 1)
+ 
