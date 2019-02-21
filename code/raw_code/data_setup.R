2061be4bab2681f7ac1a229c28f6054df23eeb2e
Ken Morales <kmorales@problemforward.com>
2019-02-21 19:24:38 +0000

    Half of the cops' cases

--- code/raw_code/data_setup.R
+++ code/raw_code/data_setup.R
@@ -35,3 +35,66 @@ save(mdcs_rayam,
      file = here("data/tidy_data",
      "mdcs_rayam.rda"))
 
+# Ward
+
+mdcs_ward <- c()
+
+for(i in 2008:2017){
+  mdcs_ward <- bind_rows(mdcs_ward,
+                         read_csv(here("data/raw_data",
+                                        str_c("Ward_", i, ".csv")),
+                                  col_names = FALSE))
+}
+
+save(mdcs_ward, 
+     file = here("data/tidy_data",
+                 "mdcs_ward.rda"))
+
+# Hersl
+
+mdcs_hersl <- c()
+
+for(i in 2008:2017){
+  mdcs_hersl <- bind_rows(mdcs_hersl,
+                         read_csv(here("data/raw_data",
+                                       str_c("Hersl_", i, ".csv")),
+                                  col_names = FALSE))
+}
+
+save(mdcs_hersl, 
+     file = here("data/tidy_data",
+                 "mdcs_hersl.rda"))
+
+# Taylor
+# NOTE: searched for "Marcus R Taylor"
+# NOTE: no entries for 2008-2009
+
+mdcs_taylor <- c()
+
+for(i in 2010:2017){
+  mdcs_taylor <- bind_rows(mdcs_taylor,
+                          read_csv(here("data/raw_data",
+                                        str_c("Taylor_", i, ".csv")),
+                                   col_names = FALSE))
+}
+
+save(mdcs_taylor, 
+     file = here("data/tidy_data",
+                 "mdcs_taylor.rda"))
+
+# Clewell
+# NOTE: no entries for 2008
+
+mdcs_clewell <- c()
+
+for(i in 2009:2017){
+  mdcs_clewell <- bind_rows(mdcs_clewell,
+                            read_csv(here("data/raw_data",
+                                          str_c("Clewell_", i, ".csv")),
+                                    col_names = FALSE))
+}
+
+save(mdcs_clewell, 
+     file = here("data/tidy_data",
+                 "mdcs_clewell.rda"))
+
